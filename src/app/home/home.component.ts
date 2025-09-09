11e704c3752543a23a833346d5a7d3f751505ed2
mcnudi <mcnudi@hotmail.com>
2025-09-09 11:55:29 +0200

    v3- landing page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,24 +1,75 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, inject, ViewChild } from '@angular/core';
 import { NombreComponent } from '../nombre/nombre.component';
 import { ContactoComponent } from '../contacto/contacto.component';
 import { SobremiComponent } from '../sobremi/sobremi.component';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { ExperienciaComponent } from '../experiencia/experiencia.component';
+import { TestimoniosComponent } from '../testimonios/testimonios.component';
 
 @Component({
   selector: 'app-home',
-  imports: [NombreComponent,ContactoComponent,SobremiComponent,RouterLink,ExperienciaComponent],
+  imports: [NombreComponent,ContactoComponent,SobremiComponent,RouterLink,ExperienciaComponent, TestimoniosComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
+
+
 
   
 })
 export class HomeComponent {
-  icono:string="";
+  icono:string='';
+  
+  router = inject(Router);
+  @ViewChild('canvas') canvas!: ElementRef<HTMLCanvasElement>;
+   ngAfterViewInit(): void {
+    const canvasEl = this.canvas.nativeElement;
+  const ctx = canvasEl.getContext('2d');
+  if (!ctx) return;
 
+  const texto = "Desarrolladora full stack Java, Spring Boot, Angular";
+  let x = canvasEl.width; // arranca desde la derecha
+  const speed = 3; // velocidad
+
+  const fontSize = Math.floor(canvasEl.width / 25);
+  ctx.font = `${fontSize}px Great Vibes`;
+  ctx.fillStyle = "#95491d";
+
+  const textWidth = ctx.measureText(texto).width;
+  const y = canvasEl.height / 2 + fontSize / 2;
+
+  const animate = () => {
+    // limpiar canvas
+    ctx.clearRect(0, 0, canvasEl.width, canvasEl.height);
+
+    // dibujar texto en la posición actual
+    ctx.fillText(texto, x, y);
+
+    // actualizar posición
+    x -= speed;
+
+    // reiniciar si salió de la pantalla
+    if (x < -textWidth) {
+      x = canvasEl.width;
+    }
+
+    requestAnimationFrame(animate);
+  };
+
+  animate();
+  }
   mostrar(valor:string){
-    if (valor==="contacto" || valor==="sobremi"|| valor==="experiencia")
-      console.log(valor);
+    
       this.icono=valor;
+    if (valor==="sobremi"){
+      //this.router.navigate(['/sobremi']);
+    }else if (valor==="experiencia"){
+      //this.router.navigate(['/experiencia']);
+    }
+    
 }
+  
+ngOnInit(): void {
+    //Aquí le dices qué quieres mostrar por defecto
+    this.icono = 'test'; 
+  }      
 }
