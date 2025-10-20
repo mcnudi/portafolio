63bade1e12d7020b4c19ea255d734fabf25a0eff
mcnudi <mcnudi@hotmail.com>
2025-10-20 19:18:30 +0200

    arreglo la parte de la experiencia

--- src/app/pages/experiencia/experiencia.component.ts
+++ src/app/pages/experiencia/experiencia.component.ts
@@ -3,11 +3,11 @@ import { CommonModule } from '@angular/common';
 import { Iexperiencia } from '../../interfaces/iexperiencia';
 import { MatDialog } from '@angular/material/dialog';
 import { DescripcionComponent } from '../descripcion/descripcion.component';
+import { ExperienciaServiceService } from '../../services/experiencia-service.service';
 
 
 @Component({
   selector: 'app-experiencia',
-  //imports: [CommonModule, TimelineModule],
   imports: [CommonModule],
   templateUrl: './experiencia.component.html',
   styleUrl: './experiencia.component.css'
@@ -16,60 +16,8 @@ export class ExperienciaComponent {
 
   i:number=0;
   private dialog = inject (MatDialog);
-  experiencias: Iexperiencia[] = [
-    {
-      empresa: 'Eviden (Grupo Atos)',
-      cliente: 'Canal de Isabel II',
-      fecha: 'Mar 2024',
-      cargo: 'Programadora Java',
-      descripcion: `
-        - Desarrollé microservicios REST con JPA y SQL.
-        - Participé en reuniones ágiles semanales.
-      `
-    },
-    {
-      empresa: 'Eviden (Grupo Atos)',
-      cliente: 'Aguas de Barcelona',
-      fecha: 'Ago 2022',
-      cargo: 'Programadora Java Swing',
-      descripcion: `- Resolví incidencias y optimicé la integración con servicios SOAP y consultas SQL.
-                    - Mantenimiento y evolución de aplicaciones en Java Swing y Struts.`
-    },
-    {
-      empresa: 'Atos',
-      cliente: 'Ayesa / Endesa Portugal',
-      fecha: 'Feb 2022',
-      cargo: 'Programadora Java',
-      descripcion: `-Desarrollé nuevas funcionalidades en aplicaciones de facturación energética utilizando Java y SQL.`
-    },
-    {
-      empresa: 'Atos',
-      cliente: 'AENA',
-      fecha: 'Ago 2021',
-      cargo: 'Programadora Java',
-      descripcion: `- Mantenimiento de aplicaciones en Java con Spring y Struts, desplegadas en Weblogic y Wildfly,`
-    },
-    {
-      empresa: 'Atos',
-      cliente: 'Aliseda / Banco Popular',
-      fecha: 'Abr 2015',
-      cargo: 'Analista funcional y técnica',
-      descripcion: `- Gestioné procesos DTSX para sincronización con Salesforce, liderando tres migraciones que redujeron
-considerablemente los tiempos de procesamiento.
-- Recogí requisitos funcionales, coordiné con usuarios y documenté procesos para facilitar la mantenibilidad.`
-    },
-    {
-      empresa: 'Atos',
-      cliente: 'Bankia',
-      fecha: 'Oct 2012',
-      cargo: 'Coordinadora y analista/programadora Java Swing',
-      descripcion: `- Coordiné tareas del equipo técnico en la factoría ATOS, asegurando entregas a tiempo.
-- Desarrollé nuevas funcionalidades para una aplicación de escritorio con Java Swing.
-- La lógica de negocio de la aplicación estaba conectada a procesos en host, con los que
-interactuábamos para consultar y modificar información bancaria en tiempo real.`
-    }
-    
-  ];
+  experienciasServ = inject(ExperienciaServiceService);
+  experiencias=this.experienciasServ.getExperiencia();
 
 ngOnInit():void{
   this.experiencias.sort((a,b)=>
--- src/app/services/experiencia-service.service.ts
+++ src/app/services/experiencia-service.service.ts
@@ -1,9 +1,65 @@
 import { Injectable } from '@angular/core';
+import { Iexperiencia } from '../interfaces/iexperiencia';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ExperienciaServiceService {
-
-  constructor() { }
-}
+    private experiencias: Iexperiencia[] = [
+    {
+      empresa: 'Eviden (Grupo Atos)',
+      cliente: 'Canal de Isabel II',
+      fecha: 'Mar 2024',
+      cargo: 'Programadora Java',
+      descripcion: `
+        - Desarrollé microservicios REST con JPA y SQL.
+        - Participé en reuniones ágiles semanales.
+      `
+    },
+    {
+      empresa: 'Eviden (Grupo Atos)',
+      cliente: 'Aguas de Barcelona',
+      fecha: 'Ago 2022',
+      cargo: 'Programadora Java Swing',
+      descripcion: `- Resolví incidencias y optimicé la integración con servicios SOAP y consultas SQL.
+                    - Mantenimiento y evolución de aplicaciones en Java Swing y Struts.`
+    },
+    {
+      empresa: 'Atos',
+      cliente: 'Ayesa / Endesa Portugal',
+      fecha: 'Feb 2022',
+      cargo: 'Programadora Java',
+      descripcion: `-Desarrollé nuevas funcionalidades en aplicaciones de facturación energética utilizando Java y SQL.`
+    },
+    {
+      empresa: 'Atos',
+      cliente: 'AENA',
+      fecha: 'Ago 2021',
+      cargo: 'Programadora Java',
+      descripcion: `- Mantenimiento de aplicaciones en Java con Spring y Struts, desplegadas en Weblogic y Wildfly,`
+    },
+    {
+      empresa: 'Atos',
+      cliente: 'Aliseda / Banco Popular',
+      fecha: 'Abr 2015',
+      cargo: 'Analista funcional y técnica',
+      descripcion: `- Gestioné procesos DTSX para sincronización con Salesforce, liderando tres migraciones que redujeron
+considerablemente los tiempos de procesamiento.
+- Recogí requisitos funcionales, coordiné con usuarios y documenté procesos para facilitar la mantenibilidad.`
+    },
+    {
+      empresa: 'Atos',
+      cliente: 'Bankia',
+      fecha: 'Oct 2012',
+      cargo: 'Coordinadora y analista/programadora Java Swing',
+      descripcion: `- Coordiné tareas del equipo técnico en la factoría ATOS, asegurando entregas a tiempo.
+- Desarrollé nuevas funcionalidades para una aplicación de escritorio con Java Swing.
+- La lógica de negocio de la aplicación estaba conectada a procesos en host, con los que
+interactuábamos para consultar y modificar información bancaria en tiempo real.`
+    }
+    
+  ];
+  getExperiencia():Iexperiencia[]{
+    return this.experiencias;
+  }
+  }
